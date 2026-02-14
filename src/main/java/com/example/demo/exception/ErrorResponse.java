994cc1d48dfb645afd93a95aa6598dede47814e0
vitor3211 <vitorlscosta123@gmail.com>
2026-02-14 16:52:00 -0300

    Ajustando ErrorResponse

--- src/main/java/com/example/demo/exception/ErrorResponse.java
+++ src/main/java/com/example/demo/exception/ErrorResponse.java
@@ -3,15 +3,10 @@ package com.example.demo.exception;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-public class ErrorResponse {
-    private final LocalDateTime timestamp;
-    private final String message;
-    private final Throwable throwable;
-    private final HttpStatus httpStatus;
-    private final String path;
+public record ErrorResponse(LocalDateTime timestamp,
+                            String message,
+                            Throwable throwable,
+                            HttpStatus httpStatus,
+                            String path) {
 }
